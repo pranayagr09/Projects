e1b47d33146a02bc41e75d407f8e214cc803469e
pranay agrawal <pranay.agr09@gmail.com>
2017-04-08 17:00:59 +0530

    CS101 Project

---
+++ GestureControlledSignaltransmission-ITSP/Receiver_code.ino
@@ -0,0 +1,149 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+#include<Servo.h>
+
+typedef struct{
+  int yaw;
+  int pitch;
+  int roll;
+  int throttle;
+}
+Quad_R;
+
+int yaw_old=0;
+int pitch_old=0;
+int roll_old=0;
+int throttle_old=0;
+int yaw_transmit=0;
+int pitch_transmit=0;
+int roll_transmit=0;
+int throttle_transmit=0;
+int i=0;
+int j=1;
+int m=j;
+//i,j and m are variables defined so as to calibrate flightmode in OpenPilot software as it will not detect it otherwise
+ 
+Quad_R reading ;
+RF24 radio(7,8);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+Servo yaw;
+Servo pitch;
+Servo roll;
+Servo throttle;
+Servo Flightmode;
+
+
+void setup(void){
+  yaw.attach(3);//gives PWM output of yaw of Servo type on pin 3
+  pitch.attach(5);//gives PWM output of pitch of Servo type on pin 5
+  roll.attach(6);//gives PWM output of roll of Servo type on pin 6
+  throttle.attach(9);//gives PWM output of throttle of Servo type on pin 9
+  Flightmode.attach(10);//gives PWM output of flightmode of Servo type on pin 10
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+  radio.startListening();
+}
+
+ 
+void loop(void){
+  if (radio.available()){
+    bool done=false;
+    while(!done){
+      i++;
+      radio.read(&reading,sizeof(reading));//data received at receiver as a struct
+     
+      if(reading.yaw>=0){
+         reading.yaw=reading.yaw/10;
+         reading.yaw=reading.yaw*10;
+      }
+      else{
+        reading.yaw=reading.yaw/10;
+        reading.yaw=reading.yaw*10-10;
+        
+      }
+      if(reading.pitch>=0){
+         reading.pitch=reading.pitch/10;
+         reading.pitch=reading.pitch*10;
+      }
+      else{
+        reading.pitch=reading.pitch/10;
+        reading.pitch=reading.pitch*10-10;
+        
+      }
+      if(reading.roll>=0){
+         reading.roll=reading.roll/10;
+         reading.roll=reading.roll*10;
+      }
+      else{
+        reading.roll=reading.roll/10;
+        reading.roll=reading.roll*10-10;
+        
+      }
+      reading.throttle=reading.throttle/10;
+      reading.throttle=reading.throttle*10;
+     //the procedure above converts the reading of yaw, pitch, roll and throttle in multiples of 10
+      
+      if(reading.throttle!=0 && abs(reading.yaw-yaw_old)<=20 && abs(reading.pitch-pitch_old)<=20 && abs(reading.roll-roll_old)<=20){
+        yaw_old=reading.yaw;
+        pitch_old=reading.pitch;
+        roll_old=reading.roll;
+        throttle_old=reading.throttle;
+      }
+      //this is done to avoid fluctuations in readings as in that case it will take the previous reading 
+      
+      Serial.print(yaw_old);                     
+      Serial.print("\t");
+      Serial.print(pitch_old);
+      Serial.print("\t");
+      Serial.print(roll_old);
+      Serial.print("\t");
+      Serial.print(throttle_old);
+      Serial.println("\t");
+      
+      yaw_transmit=yaw_old;
+      roll_transmit=roll_old;
+      pitch_transmit=pitch_old;
+      throttle_transmit=throttle_old;
+      //this assignment is done to ensure that the values don`t get affected as map and constrain functions in Servo library  can change them and could result in junk values
+
+      //Constraints setup
+      yaw_transmit=constrain(yaw_transmit,-60,60);
+      pitch_transmit=constrain(pitch_transmit,-80,80);
+      roll_transmit=constrain(roll_transmit,-90,90);
+      throttle_transmit=constrain(throttle_transmit,825,915);
+
+      //Mapping for Servo
+      yaw_transmit=map(yaw_transmit,-60,60,0,180);
+      pitch_transmit=map(pitch_transmit,-80,80,0,180);
+      roll_transmit=map(roll_transmit,-90,90,0,180);
+      throttle_transmit=map(throttle_transmit,825,915,0,180);
+      
+      //Flight Mode PWM Output 
+      m=j;
+      m=map(m,1,3,0,180);
+      if((i/10)%2 == 0)
+      {
+        if(j==3) j=1;
+        else j++;
+      }
+      else 
+      {
+        j=2;
+      }
+      Serial.println(j);
+      Serial.println("\t");
+      
+      //Witing data on Servo Variables
+      yaw.write(yaw_transmit);
+      pitch.write(pitch_transmit);
+      roll.write(roll_transmit);
+      throttle.write(throttle_transmit);
+      Flightmode.write(m);
+      
+      delay(100);//done so as to match the frequency of data being transmitted and received
+    }
+  }
+}
---
+++ GestureControlledSignaltransmission-ITSP/transmitter_code.ino
@@ -0,0 +1,207 @@
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+#include <nRF24L01.h>    //header file for transceiver 
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+float yaw_old;
+float pitch_old;
+float roll_old;
+int i=0;
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+typedef struct{
+  int yaw;
+  int pitch ;
+  int roll;
+  int throttle;
+}
+Quad_R;
+
+Quad_R reading;
+RF24 radio(7,8);
+const uint64_t pipe = 0xE8E8F0F0E1LL;   
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+     radio.begin();       // to make transceiver on
+     radio.openWritingPipe(pipe);
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+   
+    // initialize device
+    //Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+ devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        mpu.setDMPEnabled(true);
+
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    
+    int flexSensorReading = analogRead(A0);
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+     
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        i++;
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            ypr[0]=ypr[0] * 180/M_PI;
+            ypr[1]=ypr[1] * 180/M_PI;
+            ypr[2]=ypr[2] * 180/M_PI;
+        #endif
+      
+        
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+        // further code to make all the readings 0(except flex sensor reading)after mpu readings get stabilised.
+        if(i<1500){             
+          reading.yaw=ypr[0];
+          reading.pitch=ypr[1];
+          reading.roll=ypr[2];
+          reading.throttle=flexSensorReading;
+          radio.write(&reading, sizeof(reading));
+         }
+        if(i==1500){
+          yaw_old=ypr[0];
+          pitch_old=ypr[1];
+          roll_old=ypr[2];
+        }
+        if(i>1500){
+          reading.yaw=ypr[0]-yaw_old;
+          reading.pitch=ypr[1]-pitch_old;
+          reading.roll=ypr[2]-roll_old;
+          reading.throttle=flexSensorReading;
+          radio.write(&reading, sizeof(reading));
+        }
+       
+ // delay(100);
+    }
+}
+
